d4e35c61fef3c8e37cf168e70f7bab846d889daa
Reeya Gupta <guptareeya53@gmail.com>
2024-08-15 09:45:03 -0400

    Add files

---
+++ api/create_api_container.py
@@ -0,0 +1,88 @@
+import sys
+import argparse
+import subprocess
+import json
+
+def load_json(filepath):
+    with open(filepath, 'r') as f:
+        return json.load(f)
+
+def main():
+    ### handle command line arguments
+    parser = argparse.ArgumentParser(
+        prog="create_api_container.py",
+        usage="python create_api_container.py [options] server",
+    )
+    parser.add_argument("-s", "--server", help="tst/prd")
+    options = parser.parse_args()
+    if not options.server or options.server not in {"tst", "prd"}:
+        parser.print_help()
+        sys.exit(1)
+    server = options.server
+
+    ### get config info for docker container creation
+    config_obj = load_json("config.json")
+    if not isinstance(config_obj, dict):
+        print("Invalid config object type, expected dict.")
+        sys.exit(1)
+    
+    # Customize these lines with project-specific details
+    api_image = f"{config_obj['project']}_api_{server}"
+    api_container_name = f"running_{api_image}"
+    mongo_container_name = f"running_{config_obj['project']}_mongo_{server}"
+    api_port = config_obj["api_port"][server]
+    db_name = config_obj["dbinfo"]["dbname"]
+    data_path = config_obj["data_path"]
+    mongo_network_name = f"{config_obj['dbinfo']['bridge_network']}_{server}"
+    db_user = config_obj["dbinfo"][db_name]["user"]
+    db_pass = config_obj["dbinfo"][db_name]["password"]
+    conn_str = f"mongodb://{db_user}:{db_pass}@{mongo_container_name}:27017/?authSource={db_name}"
+
+    ### create and populate command list
+    cmd_list = []
+
+    # command to package the API
+    # This assumes your project has a setup.py for building a wheel
+    # If not needed, remove the following line
+    cmd_list.append("python setup.py bdist_wheel")
+
+    # Build Docker image
+    cmd_list.append(f"docker build -t {api_image} .")
+
+    # Check if the container exists and remove it
+    container_id = (
+        subprocess.getoutput(f"docker ps --all | grep {api_container_name}")
+        .split(" ")[0]
+        .strip()
+    )
+    if container_id.strip() != "":
+        cmd_list.append(f"docker rm -f {container_id}")
+
+    # Create the API Docker container
+    cmd = f"docker create --name {api_container_name} --network {mongo_network_name} -p 127.0.0.1:{api_port}:80"
+    cmd += f" -v {data_path}:{data_path} -v /software/pipes:/hostpipe -e MONGODB_CONNSTRING={conn_str} -e DB_NAME={db_name}"
+    cmd += f" -e DATA_PATH={data_path} -e SERVER={server} {api_image}"
+    cmd_list.append(cmd)
+
+    def run_command(cmd):
+        result = subprocess.run(
+            cmd,
+            shell=True,
+            text=True,
+            encoding="utf-8",
+            errors="replace",
+            capture_output=True,
+        )
+        if result.returncode != 0:
+            print(
+                f"Command failed with error code {result.returncode}: {result.stderr}"
+            )
+        else:
+            print(result.stdout)
+
+    # Run the commands
+    for cmd in cmd_list:
+        run_command(cmd)
+
+if __name__ == "__main__":
+    main()
---
+++ api/create_mongodb_container.py
@@ -0,0 +1,77 @@
+import sys
+import argparse
+import subprocess
+import json
+def load_json(filepath):
+    with open(filepath, 'r') as f:
+        return json.load(f)
+def main():
+    ### Handle command line arguments
+    parser = argparse.ArgumentParser(
+        prog="create_mongodb_container.py", usage="python create_mongodb_container.py [options] server"
+    )
+    parser.add_argument("-s", "--server", help="tst/prd")
+    options = parser.parse_args()
+    if not options.server or options.server not in {"tst", "prd"}:
+        parser.print_help()
+        sys.exit(1)
+    server = options.server
+
+    ### Get config info for docker container creation
+    config_obj = load_json("config.json")
+    if not isinstance(config_obj, dict):
+        print(
+            f"Error reading config JSON, expected type dict and got {type(config_obj)}."
+        )
+        sys.exit(1)
+
+
+
+####
+# Project specific variables
+    project_name = config_obj['project']
+    api_container_name = f"running_{project_name}_api_{server}"
+    mongo_container_name = f"running_{project_name}_mongo_{server}"
+    mongo_network_name = f"{project_name}_network_{server}"
+    mongo_port = config_obj["dbinfo"]["mongodb"]["port"]
+    data_path = config_obj["data_path"]
+    username = config_obj["dbinfo"]["admin"]["user"]
+    password = config_obj["dbinfo"]["admin"]["password"]
+    e_params = ""
+    if username and password:
+        e_params = f"-e MONGO_INITDB_ROOT_USERNAME={username} -e MONGO_INITDB_ROOT_PASSWORD={password}"
+
+    ### Create and populate command list
+    cmd_list = []
+
+    # Check if containers already exist (whether running or in a stopped state)
+    for c in {api_container_name, mongo_container_name}:
+        cmd = f"docker ps --all | grep {c}"
+        container_id = subprocess.getoutput(cmd).split(" ")[0].strip()
+        if container_id.strip() != "":
+            print(f"Found container: {c}")
+            cmd_list.append(f"docker rm -f {container_id}")
+
+    # Check if docker network already exists
+    network_cmd = f"docker network ls | grep {mongo_network_name}"
+    network_cmd_output = subprocess.getoutput(network_cmd).split()
+    if network_cmd_output != []:
+        if network_cmd_output[1] == mongo_network_name:
+            print(f"Found network: {network_cmd_output[1]}")
+            cmd_list.append(f"docker network rm {mongo_network_name} | true")
+
+    # Create docker network command
+    cmd_list.append(f"docker network create -d bridge {mongo_network_name}")
+
+    # Create MongoDB container command
+    mongo_cmd = f"docker create --name {mongo_container_name} --network {mongo_network_name} -p 127.0.0.1:{mongo_port}:27017"
+    mongo_cmd += f" -v {data_path}/api_db/{server}:/data/db {e_params} mongo"
+    cmd_list.append(mongo_cmd)
+
+    # Run the commands
+    for cmd in cmd_list:
+        x = subprocess.getoutput(cmd)
+        print(x)
+
+if __name__ == "__main__":
+    main()
